a0b2c3f96f349ecebfc7302efa31ccdaef0aee85
AdeleGropplero <adele.gdt@hotmail.com>
2024-12-07 11:17:56 +0100

    secondo commit

--- src/Components/MyFooter.jsx
+++ src/Components/MyFooter.jsx
@@ -1,7 +1,10 @@
 const MyFooter = () => (
-  <footer className="p-3 fixed-bottom footer text-center ">
+  <footer className="p-3 text-center pt-5 ">
     <p>&copy;2024 Meteo Epico. </p>
   </footer>
 );
 
 export default MyFooter;
+
+
+https://media.gettyimages.com/id/1319906466/it/foto/veduta-aerea-della-citt%C3%A0-di-milano-con-parco-sempione-italia.jpg?s=612x612&w=0&k=20&c=lC2cxz7ovxJxJuWC4y-KqtAqtvgW5mP0AE4JL48NCXc=
