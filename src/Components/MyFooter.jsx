d92e3d548ac5f93c359bc46cac34718f3e54b0ab
AdeleGropplero <adele.gdt@hotmail.com>
2024-12-07 11:18:21 +0100

    terzo

--- src/Components/MyFooter.jsx
+++ src/Components/MyFooter.jsx
@@ -5,6 +5,3 @@ const MyFooter = () => (
 );
 
 export default MyFooter;
-
-
-https://media.gettyimages.com/id/1319906466/it/foto/veduta-aerea-della-citt%C3%A0-di-milano-con-parco-sempione-italia.jpg?s=612x612&w=0&k=20&c=lC2cxz7ovxJxJuWC4y-KqtAqtvgW5mP0AE4JL48NCXc=
